25a831eed493dd7b1282a91004b3ebdbdc67ac09
george francis <georgefrancis105@gmail.com>
2017-04-28 21:33:01 +1200

    Get and post netatmo device data to firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-
+var netatmo = require('netatmo');
 var serviceAccount = require('./firebase-admin.key.json');
 
 admin.initializeApp({
@@ -7,13 +7,42 @@ admin.initializeApp({
   databaseURL: 'https://angle-control.firebaseio.com'
 });
 
-var senors;
+var remoteSenors = [];
 
 var db = admin.database();
 var sensorsRef = db.ref('sensors');
 
+console.log('starting ioatt-firebase-server-worker');
+
 sensorsRef.on('value', sensorRefs => {
     let sensorsVal =sensorRefs.val();
-    sensors = Object.keys(sensorsVal).map(sensor => sensorsVal[sensor]);
-    console.log('sensors', sensors);
-})
+    remoteSenors = Object.keys(sensorsVal)
+        .map(key => {
+            let sensor = sensorsVal[key];
+            sensor.key = key;
+            return sensor;
+        })
+        .filter(sensor => sensor.type === 'remote');
+    updateSensors();
+    
+});
+
+function updateSensors() {
+    remoteSenors.forEach(sensor => {
+        getSensorData(sensor);
+    })
+}
+
+function getSensorData(sensor) {
+    var api = new netatmo(sensor.auth);
+    api.getStationsData(function(err, devices) {
+        let data = devices[0].dashboard_data;
+        console.log('devices', devices);
+        postSensorData(sensor.key, data);
+    });
+}
+
+function postSensorData(key, data) {
+    console.log('posting data', data, 'to', key);
+    db.ref(`sensors/${key}/data`).set(data);
+}
