59ee7b110ffe3780c21f228ab53a145590c9d650
george francis <georgefrancis105@gmail.com>
2017-04-29 18:00:50 +1200

    check some rules

--- index.js
+++ index.js
@@ -8,24 +8,41 @@ admin.initializeApp({
 });
 
 var remoteSenors = [];
+var rules = [];
 
 var db = admin.database();
 var sensorsRef = db.ref('sensors');
+var rulesRef = db.ref('rules');
 
 console.log('starting ioatt-firebase-server-worker');
 
 sensorsRef.on('value', sensorRefs => {
-    let sensorsVal =sensorRefs.val();
+    let sensorsVal = sensorRefs.val();
     remoteSenors = Object.keys(sensorsVal)
         .map(key => {
             let sensor = sensorsVal[key];
             sensor.key = key;
             return sensor;
         })
-        .filter(sensor => sensor.type === 'remote');
-    updateSensors();
-    
+        .filter(sensor => sensor.type === 'remote');    
 });
+
+rulesRef.on('value', ruleRefs => {
+    let rulesVal = ruleRefs.val();
+    rules = Object.keys(rulesVal)
+        .map(key => {
+            let rule = rulesVal[key];
+            rule.key = key;
+            return rule;
+        });    
+});
+
+setTimeout(updateAllThings, 5000);
+
+function updateAllThings() {
+    updateSensors();
+    recalculateRules();
+}
 
 function updateSensors() {
     remoteSenors.forEach(sensor => {
@@ -37,12 +54,72 @@ function getSensorData(sensor) {
     var api = new netatmo(sensor.auth);
     api.getStationsData(function(err, devices) {
         let data = devices[0].dashboard_data;
-        console.log('devices', devices);
         postSensorData(sensor.key, data);
     });
 }
 
 function postSensorData(key, data) {
-    console.log('posting data', data, 'to', key);
+    console.log('updating sensor data');
     db.ref(`sensors/${key}/data`).set(data);
+}
+
+function recalculateRules() {
+    rules.forEach(rule => {
+        calculatedValue = rule.conditions.reduce(calculateContionalCheck, true);
+        console.log('calculated Value', calculatedValue);
+    });
+}
+
+function calculateContionalCheck(previousValue, currentCondition) {
+    if (currentCondition) {
+        if (currentCondition.condionalOperator === 'or') {
+            return previousValue || calculateRuleType(currentCondition);
+        }
+
+        if (currentCondition.condionalOperator) {
+            return previousValue && calculateRuleType(currentCondition);
+        }
+        return previousValue && calculateRuleType(currentCondition);
+    }
+    return true;
+}
+
+function calculateRuleType(condition) {
+    if (condition.type === 'time') {
+        return calculateTimeCondition(condition);
+    }
+
+    if (condition.type === 'temperature') {
+        return calculateTemperatureCondition(condition);
+    }
+
+    return false;
+}
+
+function calculateTimeCondition(condition) {
+    let startTime = getDateFromTime(condition.startTime);
+    let endTime = getDateFromTime(condition.endTime);
+    let currentTime = Date.now();
+
+    return currentTime > startTime && currentTime < endTime;
+}
+
+function calculateTemperatureCondition(condition) {
+    console.log('calculating temp condition', condition);
+
+    db.ref(`sensors/${condition.sensor}`).once('value')
+    .then(sensorRef => {
+        let sensor = sensorRef.val();
+        let valueToCheck = sensor.data[condition.sensorKey];
+        console.log('sensor valueToCheck', valueToCheck);
+    })
+    return true;
+}
+
+function getDateFromTime(time) {
+    let timeArr = time.split(':');
+    let date = new Date();
+    date.setHours(timeArr[0]);
+    date.setMinutes(timeArr[1]);
+    return date;
 }
