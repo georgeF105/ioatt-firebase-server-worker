e2e1ea424ddfc24b39c72a3168699e4babdaaf8f
george francis <georgefrancis105@gmail.com>
2017-06-11 14:52:23 +1200

    Make rule condition interfaces

--- src/rules/IRule.ts
+++ src/rules/IRule.ts
@@ -1,8 +1,11 @@
 import { IRuleCondition } from './IRuleCondition';
+import { IWeekDayRuleCondition } from './IWeekDayRuleCondition';
+import { ITemperatureRuleCondition } from './ITemperatureRuleCondition';
+import { ITimeRuleCondition } from './ITimeRuleCondition';
 
 export interface IRule {
   active: boolean;
-  conditions: Array<IRuleCondition>;
+  conditions: Array<ITemperatureRuleCondition | ITimeRuleCondition | IWeekDayRuleCondition>;
   type: string;
   name: string;
   linkedDeviceKey: string;
